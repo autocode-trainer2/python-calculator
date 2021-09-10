dc12f5bb6e04a57877b025a8c39e339ad826b5f6
Kirill Peshin <kirill_peshin@epam.com>
2021-09-10 15:01:28 +0300

    01

---
+++ calculator.py
@@ -0,0 +1,17 @@
+class Calculator:
+
+  def __init__(self, x1, x2):
+    self.x1 = x1
+    self.x2 = x2
+
+  def add(self):
+    return self.x1 + self.x2
+
+  def multiply(self):
+    return self.x1 * self.x2
+
+  def subtract(self):
+    return self.x1 - self.x2
+
+  def divide(self):
+    return self.x1 / self.x2
