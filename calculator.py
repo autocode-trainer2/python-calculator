dc1cbc428ee4020926ddd208adee0f0e2d559b50
Kirill Peshin <kirill_peshin@epam.com>
2021-09-10 15:14:09 +0300

    01

---
+++ calculator.py
@@ -0,0 +1,20 @@
+class Calculator:
+
+  def __init__(self, x1, x2):
+    self.x1 = x1
+    self.x2 = x2
+
+  def add(self):
+    return self.x1 + self.x2
+
+  def multiply(self):
+    return self.x1 * self.x2
+
+  def subtract(self):
+    return self.x1 - self.x2
+
+  def divide(self):
+    return self.x1 / self.x2
+
+  def pow(self, x, n):
+    return x * self.pow(x, n + 1)
---
+++ test/test_calc.py
@@ -0,0 +1,21 @@
+from calculator import Calculator
+
+def test_add():
+  calc = Calculator(5, 7)
+  assert calc.add() == 5 + 7
+
+def test_sub():
+  calc = Calculator(5, 7)
+  assert calc.subtract() == 5 - 7
+
+def test_mul():
+  calc = Calculator(5, 7)
+  assert calc.multiply() == 5 * 7
+
+def test_div():
+  calc = Calculator(5, 7)
+  assert calc.divide() == 5 / 7
+
+def test_infinite_recursion():
+    calc = Calculator(2, 2)
+    calc.pow(2, 2)
